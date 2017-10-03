c2d08b6268b5d150c44af6182c4a90981361aaff
hoahd <hoa@squidsolutions.com>
2017-10-03 11:04:17 +0200

    store messages for offline peers and send at reconnect

--- server/server.py
+++ server/server.py
@@ -8,33 +8,68 @@ import config as cfg
 wsfromid = dict()
 
 peers= set()
+connected_peers = set()
+
+msgqueue=dict()
 
 async def consumer(websocket, message):
 	data= json.loads(message)
 	if 'login' in data : 
 		id=data['login']
-		wsfromid[id] = websocket
-		peers.add(id)
+		await login(id, websocket)
 	else :
 		if ('from' in data) and ('to' in data) and ('msg' in data) :
 			to_id =data['to']
 			if to_id in peers :
-				ws = wsfromid[to_id]
-				try :
-					await ws.send(str(message))
-				except websockets.exceptions.ConnectionClosed :
-					data={'error' : 'connection to {} closed'.format(to_id)}
-					await websocket.send(json.dumps(data))
-					del wsfromid[to_id]
+				if to_id in connected_peers :
+					ws = wsfromid[to_id]
+					try :
+						await ws.send(str(message))
+					except websockets.exceptions.ConnectionClosed :
+						await peer_offline(websocket, to_id, message)
+						del wsfromid[to_id]
+						connected_peers.remove(to_id)
+				else :
+					await peer_offline(websocket, to_id, message)
 			else :
 				data={'error' : 'peer {} does not exist'.format(to_id)}
 				await websocket.send(json.dumps(data))
 
+async def login( id, websocket) :
+	wsfromid[id] = websocket
+	peers.add(id)
+	connected_peers.add(id)
+	waiting_msg =retrieve_msg(id)
+	if not( waiting_msg is None) :
+		for msg in waiting_msg :
+			await websocket.send(msg)
+
+async def peer_offline(websockersrc, iddest, message) :
+	data={'error' : 'connection to {} closed, messages will be send as {} comes back online'.format(iddest, iddest)}
+	await websockersrc.send(json.dumps(data))
+	store_msg(iddest, str(message))
+
+
+def store_msg(id, message):
+	if id in msgqueue :
+		msgqueue[id].append(message)
+	else :
+		msgqueue[id] = [message]
+	
+
+def retrieve_msg(id) :
+	if id in msgqueue :
+		msgs=  msgqueue[id]
+		msgqueue[id] =[]
+		return msgs
+	else :
+		return None
 
 async def handler(websocket, path):
 	while True:
-		message = await websocket.recv()
-		await consumer(websocket, message)	
+		try :
+			message = await websocket.recv()
+			await consumer(websocket, message)	
 			
         
 #start_server = websockets.serve(handler, '127.0.0.1', 5678)
