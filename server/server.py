7349c6d5276df9eb274815472c7827e663658088
hoahd <hoa@squidsolutions.com>
2017-10-03 09:51:23 +0200

    handle closed ws

--- server/server.py
+++ server/server.py
@@ -7,21 +7,28 @@ import config as cfg
 
 wsfromid = dict()
 
+peers= set()
+
 async def consumer(websocket, message):
 	data= json.loads(message)
 	if 'login' in data : 
-		wsfromid[data['login']] = websocket
+		id=data['login']
+		wsfromid[id] = websocket
+		peers.add(id)
 	else :
 		if ('from' in data) and ('to' in data) and ('msg' in data) :
-		#	print( 'send '+ data['msg'] + ' from ' + data['from'] + ' to ' + data['to'])
-			ws = wsfromid[data['to']]
-			try :
-				await ws.send(str(message))
-			except websockets.exceptions.ConnectionClosed :
-				id=data['to']
-				print('connection to '  + id + ' closed')
-				del wsfromid[id]
-
+			to_id =data['to']
+			if to_id in peer :
+				ws = wsfromid[to_id]
+				try :
+					await ws.send(str(message))
+				except websockets.exceptions.ConnectionClosed :
+					data={'error' : 'connection to {} closed''.format(to_id)}
+					websocket.send(json.dumps(data))
+					del wsfromid[to_id]
+			else :
+				data={'error' : 'peer {} does not exist'.format(to_id)}
+				websocket.send(json.dumps(data))
 
 
 async def handler(websocket, path):
